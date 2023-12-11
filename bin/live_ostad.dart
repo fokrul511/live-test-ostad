808f326b63870efec8729bf48495aebbb83c4884
fokrul511 <islamfokrul2002@gmail.com>
2023-12-11 22:19:14 +0600

    live test

---
+++ bin/live_ostad.dart
@@ -0,0 +1,5 @@
+// import 'package:live_ostad/live_ostad.dart' as live_ostad;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${live_ostad.calculate()}!');
+// }
---
+++ test/live_ostad_test.dart
@@ -0,0 +1,8 @@
+// import 'package:live_ostad/live_ostad.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
